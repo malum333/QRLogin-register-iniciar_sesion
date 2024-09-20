c6b9c845a09a900e1c7dd774b25b9c0cc2eebe3f
malum333 <tuemail@example.com>
2024-09-20 12:26:48 +0000

    the sky

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Definir las rutas para los archivos estáticos
+// Servir archivos estáticos
 app.use(express.static('public'));
 
-// Ruta para la página principal (index.html)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-// Ruta para la página del casino (casino.html)
 app.get('/casino', (req, res) => {
-    res.sendFile(path.join(__dirname, 'casino.html'));
+    res.sendFile(__dirname + '/public/casino.html');
 });
 
-// Ruta para la página del hotel (hotel.html)
 app.get('/hotel', (req, res) => {
-    res.sendFile(path.join(__dirname, 'hotel.html'));
+    res.sendFile(__dirname + '/public/hotel.html');
 });
 
-// Iniciar el servidor en el puerto 3000
-app.listen(3000, () => {
-    console.log('Servidor escuchando en http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
 });
