82036d6ad32f2796c20b28ec3291a583c7e4b1c3
malum333 <tuemail@example.com>
2024-09-20 11:13:13 +0000

    firstVersion

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Definir las rutas para los archivos estáticos
+app.use(express.static('public'));
+
+// Ruta para la página principal (index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Ruta para la página del casino (casino.html)
+app.get('/casino', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'casino.html'));
+});
+
+// Ruta para la página del hotel (hotel.html)
+app.get('/hotel', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'hotel.html'));
+});
+
+// Iniciar el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
+});
